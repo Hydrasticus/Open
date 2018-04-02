d4e65ef423a29d8351cc091f9500a9b66406e3f5
Alo Pullmann <alpull@ttu.ee>
2018-04-02 20:45:50 +0300

    Lab 9 until 1.5

--- Domain/Country/CountryObjectFactory.cs
+++ Domain/Country/CountryObjectFactory.cs
@@ -5,17 +5,16 @@ using Open.Data.Country;
 namespace Open.Domain.Country {
     
     public static class CountryObjectFactory {
-        
-        public static CountryObject Create(string id, string name, string code, DateTime? validFrom = null,
-            DateTime? validTo = null) {
+        public static CountryObject Create(string id, string name, string code, 
+            DateTime? validFrom = null, DateTime? validTo = null) {
             var o = CountryDbRecordFactory.Create(id, name, code, validFrom, validTo);
             return new CountryObject(o);
         }
 
         public static CountryObject Create(RegionInfo r) {
-            var id = r.ThreeLetterISORegionName;
-            var name = r.DisplayName;
-            var code = r.TwoLetterISORegionName;
+            var id = r?.ThreeLetterISORegionName;
+            var name = r?.DisplayName;
+            var code = r?.TwoLetterISORegionName;
             return Create(id, name, code);
         }
     }
