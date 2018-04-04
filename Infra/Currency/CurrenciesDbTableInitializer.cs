dc38d335641bbe23af1388c3c43b7ba194ff2907
Alo Pullmann <alpull@ttu.ee>
2018-04-04 12:40:49 +0300

    Currencies with migrations

--- Infra/Currency/CurrenciesDbTableInitializer.cs
+++ Infra/Currency/CurrenciesDbTableInitializer.cs
@@ -1,5 +1,5 @@
-﻿using Open.Aids;
-using Open.Domain.Country;
+﻿using System.Collections.Generic;
+using Open.Aids;
 using Open.Domain.Currency;
 
 namespace Open.Infra.Currency {
@@ -9,10 +9,13 @@ namespace Open.Infra.Currency {
         public static void Initialize(ICurrencyObjectsRepository c) {
             if (c.IsInitialized()) return;
             var regions = SystemRegionInfo.GetRegionsList();
+            var l = new List<string>();
             foreach (var r in regions) {
                 if (!SystemRegionInfo.IsCountry(r)) continue;
                 var e = CurrencyObjectFactory.Create(r);
+                if (l.Contains(e.DbRecord.ID)) continue;
                 c.AddObject(e);
+                l.Add(e.DbRecord.ID);
             }
         }
     }
