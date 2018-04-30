1a639103cb7c2ed8098fbd1f952cb3ee43722a48
Alo Pullmann <alpull@ttu.ee>
2018-05-01 02:20:19 +0300

    Added migrations to MoneyDbContext

--- Infra/Migrations/20180430231833_Money.Designer.cs
+++ Infra/Migrations/20180430231833_Money.Designer.cs
@@ -10,9 +10,10 @@ using System;
 namespace Open.Infra.Migrations
 {
     [DbContext(typeof(MoneyDbContext))]
-    partial class CurrencyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180430231833_Money")]
+    partial class Money
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
