1558c3a1f69b3e11ba4483a9260352fcfba00c6d
Alo Pullmann <alpull@ttu.ee>
2018-05-02 13:31:11 +0300

    Lab 13 until p. 13

---
+++ Tests/GetUrl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Mvc;
+using Open.Aids;
+
+namespace Open.Tests {
+
+    public static class GetUrl {
+
+        public static string ForControllerAction<T>(Expression<Func<T, object>> ex = null) where T : Controller {
+            var suffix = typeof(Controller).Name;
+            var name = typeof(T).Name;
+            var controller = name.Remove(name.IndexOf(suffix, StringComparison.Ordinal));
+            var action = GetMember.Name(ex);
+            var r = string.IsNullOrEmpty(action) ? $"/{controller}" : $"/{controller}/{action}";
+            return r.ToLower();
+        }
+    }
+}
