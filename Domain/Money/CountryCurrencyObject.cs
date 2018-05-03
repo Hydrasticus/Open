073df3c18122ed5b9f05bd3ea4135022c49b9107
Alo Pullmann <alpull@ttu.ee>
2018-05-03 22:14:10 +0300

    Lab 14 until 3.5.2

---
+++ Domain/Money/CountryCurrencyObject.cs
@@ -0,0 +1,17 @@
+﻿using Open.Data.Money;
+using Open.Domain.Common;
+using Open.Domain.Location;
+
+namespace Open.Domain.Money {
+    
+    public class CountryCurrencyObject : TemporalObject<CountryCurrencyDbRecord> {
+        
+        public readonly CountryObject Country;
+        public readonly CurrencyObject Currency;
+
+        public CountryCurrencyObject(CountryCurrencyDbRecord dbRecord) : base(dbRecord) {
+            Country = new CountryObject(DbRecord.Country);
+            Currency = new CurrencyObject(DbRecord.Currency);
+        }
+    }
+}
