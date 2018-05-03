4e84ccda8e1b1924448357d50863a534b1fef517
Alo Pullmann <alpull@ttu.ee>
2018-05-03 03:21:46 +0300

    Lab 14 until p. 57

--- Infra/SentryDbContext.cs
+++ Infra/SentryDbContext.cs
@@ -7,15 +7,18 @@ namespace Open.Infra {
     public class SentryDbContext : DbContext {
 
         public SentryDbContext(DbContextOptions<SentryDbContext> o) : base(o) { }
-        
+
         public DbSet<CountryDbRecord> Countries { get; set; }
-        
+
         public DbSet<CurrencyDbRecord> Currencies { get; set; }
+
+        public DbSet<CountryCurrencyDbRecord> CountryCurrencies { get; set; }
 
         protected override void OnModelCreating(ModelBuilder b) {
             base.OnModelCreating(b);
             b.Entity<CurrencyDbRecord>().ToTable("Currency");
             b.Entity<CountryDbRecord>().ToTable("Country");
+            b.Entity<CountryCurrencyDbRecord>().ToTable("CountryCurrency").HasKey(a => new {a.CountryID, a.CurrencyID});
         }
     }
 }
