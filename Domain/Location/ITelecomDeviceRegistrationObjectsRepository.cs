596a9ec0461e47b15682567ece5c374a5e0b0cf7
Alo Pullmann <alpull@ttu.ee>
2018-05-09 19:55:09 +0300

    Lab 16 until 1.3

---
+++ Domain/Location/ITelecomDeviceRegistrationObjectsRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Open.Core;
+
+namespace Open.Domain.Location {
+    
+    public interface ITelecomDeviceRegistrationObjectsRepository : ICrudRepository<TelecomDeviceRegistrationObject> {
+
+        Task LoadAddresses(TelecomAddressObject device);
+        Task LoadDevices(GeographicAddressObject address);
+    }
+}
