79df6ffa15cc551ab996e2b0cdc556e987295ee9
Alo Pullmann <alpull@ttu.ee>
2018-05-09 10:59:09 +0300

    Lab 15 until 1.2

--- Facade/Location/TelecomAddressViewModel.cs
+++ Facade/Location/TelecomAddressViewModel.cs
@@ -1,4 +1,45 @@
-﻿namespace Open.Facade.Location {
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Open.Core;
 
-    public class TelecomAddressViewModel { }
+namespace Open.Facade.Location {
+
+    public class TelecomAddressViewModel : AddressViewModel {
+
+        private string countryCode, areaCode, number, extension, nationalDirectDialingPrefix;
+
+        [DisplayName("Country Code")]
+        public string CountryCode {
+            get => getString(ref countryCode);
+            set => countryCode = value;
+        }
+
+        [DisplayName("Area Code")]
+        public string AreaCode {
+            get => getString(ref areaCode);
+            set => areaCode = value;
+        }
+
+        public string Number {
+            get => getString(ref number);
+            set => number = value;
+        }
+
+        public string Extension {
+            get => getString(ref extension);
+            set => extension = value;
+        }
+
+        [DisplayName("National Direct Dialing Prefix")]
+        public string NationalDirectDialingPrefix {
+            get => getString(ref nationalDirectDialingPrefix);
+            set => nationalDirectDialingPrefix = value;
+        }
+
+        [DisplayName("Device Type")]
+        public TelecomDevice DeviceType { get; set; }
+
+        public List<GeographicAddressViewModel> RegisteredInAddresses { get; } =
+            new List<GeographicAddressViewModel>();
+    }
 }
