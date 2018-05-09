e2219df288fd910fbb84c659b0323fd422f3b60a
Alo Pullmann <alpull@ttu.ee>
2018-05-09 19:39:57 +0300

    Lab 16 until 1.2.10

---
+++ Domain/Location/TelecomDeviceRegistrationObject.cs
@@ -0,0 +1,16 @@
+﻿using Open.Data.Location;
+using Open.Domain.Common;
+
+namespace Open.Domain.Location {
+    
+    public class TelecomDeviceRegistrationObject : TemporalObject<TelecomDeviceRegistrationDbRecord> {
+
+        public readonly TelecomAddressObject Device;
+        public readonly GeographicAddressObject Address;
+
+        public TelecomDeviceRegistrationObject(TelecomDeviceRegistrationDbRecord dbRecord) : base(dbRecord) {
+            Address = AddressObjectFactory.Create(DbRecord.Address) as GeographicAddressObject;
+            Device = AddressObjectFactory.Create(DbRecord.Device) as TelecomAddressObject;
+        }
+    }
+}
