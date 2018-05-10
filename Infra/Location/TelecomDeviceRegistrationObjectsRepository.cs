03a4c2f0a518d81e7f2d3ca6703ff8f2b6729168
Alo Pullmann <alpull@ttu.ee>
2018-05-10 21:32:30 +0300

    Lab 16 until 1.4.2

---
+++ Infra/Location/TelecomDeviceRegistrationObjectsRepository.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Open.Data.Location;
+using Open.Domain.Location;
+
+namespace Open.Infra.Location {
+    
+    //TODO: fix
+    public class TelecomDeviceRegistrationObjectsRepository : ITelecomDeviceRegistrationObjectsRepository {
+
+        private readonly DbSet<TelecomDeviceRegistrationDbRecord> dbSet;
+        private readonly DbContext db;
+
+        public TelecomDeviceRegistrationObjectsRepository(SentryDbContext c) {
+            db = c;
+            dbSet = c?.TelecomDeviceRegistrations;
+        }
+        
+        public Task<TelecomDeviceRegistrationObject> GetObject(string id) {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task AddObject(TelecomDeviceRegistrationObject o) {
+            dbSet.Add(o.DbRecord);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task UpdateObject(TelecomDeviceRegistrationObject o) {
+            dbSet.Update(o.DbRecord);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task DeleteObject(TelecomDeviceRegistrationObject o) {
+            dbSet.Remove(o.DbRecord);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task LoadAddresses(TelecomAddressObject device) {
+            if (device is null) return;
+            var id = device.DbRecord?.ID ?? string.Empty;
+            var addresses = await dbSet.Include(x => x.Address).Where(x => x.DeviceID == id).AsNoTracking()
+                .ToListAsync();
+            foreach (var a in addresses) {
+                //device.UsedInAddress(new GeographicAddressObject(a.Address));
+            }
+        }
+
+        public async Task LoadDevices(GeographicAddressObject address) {
+            if (address is null) return;
+            var id = address.DbRecord?.ID ?? string.Empty;
+            var devices = await dbSet.Include(x => x.Device).Where(x => x.AddressID == id).AsNoTracking().ToListAsync();
+            foreach (var d in devices) {
+                //address.DeviceInUse(new TelecomAddressObject(d.Device));
+            }
+        }
+    }
+}
